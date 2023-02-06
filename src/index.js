ca47806a39417a344b8b955b59ac25c56a9b2fbf
Pavel Herzik <pavel@herzik.ru>
2023-02-06 13:43:56 +0300

    Добавлена тестовая возможность мутить пользователей. Без проверки на уровень доступа

--- src/index.js
+++ src/index.js
@@ -15,4 +15,81 @@ bot.use(sendWeather)
 bot.use(welcomeMessage)
 bot.use(farewellMessage)
 
+//===============================
+//NOTE: Реализация мута пользователе. Необходимо переписать и вынести в отдельный композер
+//===============================
+
+bot
+  .command('mute', ctx => {
+    const chatId = ctx.chat.id
+    const userId = ctx.message.reply_to_message.from.id
+    const userName = ctx.message.reply_to_message.from.first_name
+
+    bot.telegram
+      .getChatMember(chatId, userId)
+      .then(member => {
+        if (member.status === 'administrator' || member.status === 'creator') {
+          return ctx.reply('Вы не можете трогать администраторов чата!')
+        }
+
+        bot.telegram
+          .restrictChatMember(chatId, userId, {
+            can_send_messages: false,
+            can_send_media_messages: false,
+            can_send_other_messages: false,
+            can_add_web_page_previews: false,
+          })
+          .then(() =>
+            ctx.reply(`Пользователь <a href="tg://user?id=${userId}">${userName}</a> заткнулся. `, {
+              parse_mode: 'HTML',
+            })
+          )
+          .catch(error => {
+            ctx.reply('Я не могу этого сделать!')
+            console.error(error)
+          })
+      })
+      .catch(err => console.log(err))
+  })
+  .catch(err => console.log('Ошибка!', err))
+
+bot
+  .command('unmute', ctx => {
+    const chatId = ctx.chat.id
+    const userId = ctx.message.reply_to_message.from.id
+    const userName = ctx.message.reply_to_message.from.first_name
+
+    bot.telegram
+      .getChatMember(chatId, userId)
+      .then(member => {
+        if (member.status === 'administrator' || member.status === 'creator') {
+          return ctx.reply('Вы не можете трогать администраторов чата!')
+        }
+
+        bot.telegram
+          .restrictChatMember(chatId, userId, {
+            can_send_messages: true,
+            can_send_media_messages: true,
+            can_send_other_messages: true,
+            can_add_web_page_previews: true,
+          })
+          .then(() =>
+            ctx.reply(
+              `Пользователь <a href="tg://user?id=${userId}">${userName}</a> может говорить.`,
+              {
+                parse_mode: 'HTML',
+              }
+            )
+          )
+          .catch(error => {
+            ctx.reply('Я не могу этого сделать!')
+            console.error(error)
+          })
+      })
+      .then(err => console.log(err))
+  })
+  .catch(err => console.log('Ошибка!', err))
+
+//===============================
+
 bot.launch(console.log('Я живой!'))
