94ba0c21aac221b4d2a96eb9cd7d01db2ee55038
Pavel Herzik <pavel@herzik.ru>
2023-02-09 11:06:32 +0300

    Добавлен список команд

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const welcomeMessage = require('./composers/welcomeMessage.composer')
 const farewellMessage = require('./composers/farewellMessage.composer')
 const muteUser = require('./composers/muteUser.composer')
 const banUser = require('./composers/banUser.composer')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.telegram.setMyCommands([
+  { command: 'ban', description: 'Забананить' },
+  { command: 'unban', description: 'Разбананить' },
+  { command: 'mute', description: 'Замутить (лишить голоса)' },
+  { command: 'unmute', description: 'Размутить' },
+])
 
 bot.use(sendGif)
 bot.use(sendWeather)
@@ -17,3 +25,6 @@ bot.use(muteUser).catch(err => console.error(err))
 bot.use(banUser)
 
 bot.launch(console.log('Я живой!'))
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
