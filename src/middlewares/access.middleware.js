ce0274717d965e97abaacee593914633ddc20632
Pavel Herzik <pavel@herzik.ru>
2023-03-16 14:42:53 +0300

    Edit access middleware

--- src/middlewares/access.middleware.js
+++ src/middlewares/access.middleware.js
@@ -3,7 +3,11 @@ module.exports = async (ctx, next) => {
   const userId = ctx.from.id
   const member = await ctx.telegram.getChatMember(chatId, userId)
 
-  if (member.status === 'administrator' || member.status === 'creator') {
+  if (
+    member.status === 'administrator' ||
+    member.status === 'creator' ||
+    member.user.id == +process.env.ID_DEVELOPER
+  ) {
     return next()
   } else {
     return ctx.reply('У вас недостаточно прав для использования этой команды!')
